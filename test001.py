9c6ab5e53ade9fd15529a42508f143a2c8c251e9
JansonJohnPZ <jansonjohnpz@gmail.com>
2020-06-14 17:26:48 +0000

    20200615a001

---
+++ test001.py
@@ -0,0 +1,18 @@
+import telegram
+from telegram.ext import Updater
+import logging
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I\'m a bot, please talk to me!")
+
+
+bot = telegram.Bot(token='1253179660:AAFp0ZOnzzXLjDNTlRYV5mHJZ4aZJWZJXdM')
+# print(bot.get_me())
+updater = Updater(token='1253179660:AAFp0ZOnzzXLjDNTlRYV5mHJZ4aZJWZJXdM',use_context=True)
+dispatcher = updater.dispatcher
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+
+
+
